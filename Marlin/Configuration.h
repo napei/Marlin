27ae6b00b7b349ce9746d25f9d44d1face101e52
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2020-09-19 22:36:56 +1000

    offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35, 0, -2.4 }
+#define NOZZLE_TO_PROBE_OFFSET { 35, 0, -2.41 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
